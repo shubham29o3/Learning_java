e371d75c49a0e811b0fd6c427d13eefa57a4856c
shubham29o3 <97184899+shubham29o3@users.noreply.github.com>
2023-02-02 17:52:56 +0530

    Update product_of_two_number.java

--- product_of_two_number.java
+++ product_of_two_number.java
@@ -2,7 +2,7 @@ import java.util.*;
     public class product_of_two_number {
         public static void main(String args[]) {
             Scanner sc = new Scanner(System.in);
-            System.out.print("Enter a: ");
+            System.out.print("Enter a:- ");
             int a = sc.nextInt();
             System.out.print("Enter b: ");
             int b = sc.nextInt();
